6b8bd5584fc98e4075095aed73db658e3ba27dcb
Raj Mitra <indiahind320@gmail.com>
2022-07-08 21:12:38 +0530

    Second Commit

---
+++ EmployeeGitHubWebApp/Data/EmployeeGitHubWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeGitHubWebApp.Models;
+
+namespace EmployeeGitHubWebApp.Data
+{
+    public class EmployeeGitHubWebAppContext : DbContext
+    {
+        public EmployeeGitHubWebAppContext (DbContextOptions<EmployeeGitHubWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeGitHubWebApp.Models.Emp> Emp { get; set; }
+    }
+}
---
+++ EmployeeGitHubWebApp/Models/Emp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeGitHubWebApp.Models
+{
+    public class Emp
+    {
+        [Key]
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesignation { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime EDOJ { get; set; }
+    }
+}
