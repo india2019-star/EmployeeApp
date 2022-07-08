24d2ea7667a04f3104560dd0119ebe6d7b31e3da
Raj Mitra <indiahind320@gmail.com>
2022-07-08 20:53:16 +0530

    First Commit

---
+++ EmployeeGitHubWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeGitHubWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
